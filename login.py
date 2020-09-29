8d9e4842073cb61fa3af914e63c56d9dadbe43d8
Joshua Stay <57918075+joshuastay@users.noreply.github.com>
2020-09-28 17:48:05 -0700

    Add files via upload

--- login.py
+++ login.py
@@ -1,97 +1,83 @@
-'''
-Basic login script, lets you create a username and password, and test the login
-'''
-
 import re
-logindict = dict()
-status = 1
 
-# this function checks the parameters of the password
-# if all are true it returns True
-def checkpass(entry):
-    checkupper = False
-    checklower = False
-    checklen = False
-    checkint = False
-    checkspec = False
-    for each in entry:
-        if each.islower() is True:
-            checklower = True
-        else:
-            continue
-    for each in entry:
-        if each.isupper() is True:
-            checkupper = True
-        else:
-            continue
-    if re.search("\d", entry):
-        checkint = True
-    if len(entry) >= 8 and len(entry) <= 20:
-        checklen = True
-    if re.search("[!, @, #, $, %]", entry):
-        checkspec = True
-    if checkspec and checklen and checkint and checkupper and checklower:
-        return True
-    else:
-        return False
 
-# this function is used to create a new username and password
-# user is held in loop until parameters of username and password are met
-def newlogin():
-    makeuser = True
-    makepass = True
-    while makeuser is True:
-        print("Enter a new username (limit 25 characters, no spaces) ")
-        username = input("Username: ")
-        if len(username) > 25 or username.count(" ") > 0:
-            print("Invalid Username!")
-            continue
-        makeuser = False
-    while makepass is True:
-        print("Enter a new password (atleast 8 characters, limit 20. Must include lowercase, uppercase, numbers and"
-              " a special character !, @, #, $, %")
-        password = input("Enter new password: ")
-        passvalid = checkpass(password)
-        if passvalid:
-            logindict[username] = password
-            break
-        else:
-            print("Password Invalid!")
-            continue
+class LoginCredentials:
+    """
+    Simple login authentication program
+    includes a method to create a new user
+    stores values in a dictionary
+    """
 
-# this function validates credentials, it checks the dictionary for a username then checks the password for a match
-# it gives the user 3 attempts to log in
-def login():
-    username = input("Username: ")
-    if logindict.get(username) is not None:
-        attempts = 3
-        while attempts > 0:
-            password = input("Password: ")
-            if logindict[username] == password:
-                print("Login Successful!")
+    def __init__(self):
+        self.login_dict = dict()
+        self.status = 1
+
+    # method to check if the password meets parameters
+    def check_pass(self, entry):
+        check_upper = False
+        check_lower = False
+        check_length = False
+        check_int = False
+        check_spec = False
+        for each in entry:
+            if each.islower() is True:
+                check_lower = True
+            else:
+                continue
+        for each in entry:
+            if each.isupper() is True:
+                check_upper = True
+            else:
+                continue
+        if re.search("\d", entry):
+            check_int = True
+        if len(entry) >= 8 and len(entry) <= 20:
+            check_length = True
+        if re.search("[!, @, #, $, %]", entry):
+            check_spec = True
+        if check_spec and check_length and check_int and check_upper and check_lower:
+            return True
+        else:
+            return False
+
+    # new_login prompts user for a new username and password and stores values in the dictionary
+    def new_login(self):
+        make_user = True
+        make_pass = True
+        while make_user is True:
+            print("Enter a new username (limit 25 characters, no spaces) ")
+            username = input("Username: ")
+            if len(username) > 25 or username.count(" ") > 0:
+                print("Invalid Username!")
+                continue
+            elif username in self.login_dict.keys():
+                print('Username in use!')
+                continue
+            make_user = False
+        while make_pass is True:
+            print("Enter a new password (atleast 8 characters, limit 20. Must include lowercase, uppercase, numbers and"
+                  " a special character !, @, #, $, %")
+            password = input("Enter new password: ")
+            passvalid = self.check_pass(password)
+            if passvalid:
+                self.login_dict[username] = password
                 break
             else:
-                attempts -= 1
-                print("Login Failed! attempts remaining: " , attempts)
-    else:
-        print("Unrecognized Username!")
+                print("Password Invalid!")
+                continue
 
-# main line of code calling for a command to either login, make a new login or end the program
-# matches the command with the desired function
-while status > 0:
-    command = input("New, Login or Done?: ")
-
-    if command == "New":
-        newlogin()
-        continue
-
-    elif command == "Login":
-        login()
-        continue
-
-    elif command == "Done":
-        break
-
-    else:
-        print("Invalid Command!")
-
+    # login method checks the dictionary for a matching username and password
+    def login(self):
+        username = input("Username: ")
+        if self.login_dict.get(username) is not None:
+            attempts = 3
+            while attempts > 0:
+                password = input("Password: ")
+                if self.login_dict[username] == password:
+                    print("Login Successful!")
+                    break
+                else:
+                    attempts -= 1
+                    print("Login Failed! attempts remaining: ", attempts)
+        else:
+            print("Unrecognized Username!")
